eec01e5bee412b79c916b28dee8b3e3fea5313ec
Bai Li <bai@cs.toronto.edu>
2020-02-29 14:37:23 -0500

    Flag to add spaces in order to train GloVe

--- Project/scripts/extract_all_24hist.py
+++ Project/scripts/extract_all_24hist.py
@@ -1,11 +1,23 @@
-# Concatenate all of 24hist files into one file, remove metadata
+"""
+Concatenate all of 24hist files into one file, remove metadata.
+Usage:
+  PYTHONPATH=. python scripts/extract_all_24hist.py
+"""
 import glob
+import argparse
 import src.utils
 
-OUTFILE = './24hist.txt'
+parser = argparse.ArgumentParser()
+parser.add_argument('--in_dir', default='data/24hist')
+parser.add_argument('--out_file', default='./24hist.txt')
+parser.add_argument('--spaces', action='store_true') # Needed for GloVe
+args = parser.parse_args()
+print(args)
 
-with open(OUTFILE, 'w') as outf:
-  for f in glob.glob('data/24hist/*.txt'):
+with open(args.out_file, 'w') as outf:
+  for f in glob.glob(args.in_dir + '/*.txt'):
     sentences = src.utils.load_sentences(f)
     for s in sentences:
+      if args.spaces:
+        s = ' '.join(list(s))
       outf.write(s + '\n')
