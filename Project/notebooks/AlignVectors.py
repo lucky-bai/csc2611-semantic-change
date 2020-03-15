b52510185c2bd25bc0671457ae0e255d3e0d0205
Bai Li <bai@cs.toronto.edu>
2020-03-15 17:40:06 -0400

    Find characters with most and least change

--- Project/notebooks/AlignVectors.py
+++ Project/notebooks/AlignVectors.py
@@ -12,9 +12,12 @@ sys.path.append('../')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import scipy.stats
 import gensim
+import glob
 
 import src.procrustes
+import src.ud_corpus
 
 get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
@@ -53,11 +56,54 @@ vecs_modern.most_similar('也')
 vecs_modern = src.procrustes.smart_procrustes_align_gensim(vecs_classical, vecs_modern)
 
 
-# ## Print amount of change
-
 # In[7]:
 
 
-for c in list('之不也而以其子曰人者有為'):
-  print(c, np.linalg.norm(vecs_classical[c] - vecs_modern[c]))
+shared_vocab = set(vecs_classical.vocab.keys()) & set(vecs_modern.vocab.keys())
+
+
+# ## Calculate amount of change for shared vocab
+
+# In[8]:
+
+
+ud_data = src.ud_corpus.POSCorpus.create_from_ud(glob.glob('../data/UD_Classical_Chinese-Kyoto/*.conllu'))
+
+
+# In[9]:
+
+
+summary_data = ud_data.get_nv_stats().sort_values('total_count', ascending=False)
+summary_data = summary_data[summary_data.total_count >= 5]
+summary_data = summary_data[summary_data.char.isin(shared_vocab)]
+summary_data['semantic_change'] = summary_data.char.apply(
+  lambda c: np.linalg.norm(vecs_classical[c] - vecs_modern[c])
+)
+summary_data
+
+
+# ## Print words with most and least change
+
+# In[10]:
+
+
+summary_data.sort_values("semantic_change", ascending=False).head(15)
+
+
+# In[11]:
+
+
+summary_data.sort_values("semantic_change").head(15)
+
+
+# In[12]:
+
+
+sns.regplot(summary_data.noun_ratio, summary_data.semantic_change)
+
+
+# In[13]:
+
+
+scipy.stats.pearsonr(summary_data.noun_ratio, summary_data.semantic_change)
 
